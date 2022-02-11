4372e4e52bd9c024f40c5b153666a5253692380e
merve2 <73879615+merve2@users.noreply.github.com>
2022-02-12 01:53:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿    using System;
+
+namespace örn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("adınızı giriniz");
+            string name = Console.ReadLine();
+            Console.WriteLine("soyadınızı giriniz");
+            string surname = Console.ReadLine();
+            Console.WriteLine(" hoş geldiniz " + name + " " + surname);
+        }
+    }
+}
